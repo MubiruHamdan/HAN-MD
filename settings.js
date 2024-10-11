8acb90acc619853647a87f5205ba8ad49d73864b
MubiruHamdan <160257394+MubiruHamdan@users.noreply.github.com>
2024-10-11 14:53:30 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'My Sticker Pack',
   author: 'My Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "HAN-MD",
+  botOwner: 'HAMZ', // Your name
+  ownerNumber: '256766278461', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
